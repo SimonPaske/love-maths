e30fae41cc2182791e9515368c245dedd0a47d20
Simon Paske <simas.paskevicius@gmail.com>
2023-03-24 13:18:20 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-/* This code is adding event listeners to all buttons on the page and displaying an alert message when a button is clicked. The addEventListener() function is used to add an event listener for the click event on each button. The event listener is a function that checks the data-type attribute of the clicked button to determine whether it was a "submit" button or a game type button. If the button is a "submit" button, it displays an alert message saying "You clicked Submit!". If it is a game type button, it gets the value of the data-type attribute and displays an alert message with the game type that was clicked.
-
-The code is wrapped inside a DOMContentLoaded event listener to make sure that the JavaScript code is executed only after the page has finished loading. This ensures that all the necessary elements, including the buttons, are available for the code to manipulate. */
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
+
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (button.getAttribute("data-type") === "submit") {
+            if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
+
     runGame("addition");
-})
+
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -27,47 +29,52 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if (gameTypes === "addition") {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 /**
- * Check the answer against the first element in
+ * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
 
-    let userAnswer = parseInteger(document.getElementById('answer-box').value);
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert('Hey! You got it right! :D');
+        alert("Hey! You got it right! :D");
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
     }
 
     runGame(calculatedAnswer[1]);
+
 }
 
 /**
- * Gets the operands (the numbers) and operator (plus, minus etc)
- * directly from the DOM, and returns the correct answer.
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
  */
 function calculateCorrectAnswer() {
-    let operand1 = parseInt(document.getElementById("operand1").innerText);
-    let operand2 = parseInt(document.getElementById("operand2").innerText);
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
+
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else {
-        alert('Unimplemented operator ${operator}');
-        throw (`Unimplemented operator ${operator}. Aborting!`);
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
+
 }
 
 function incrementScore() {
@@ -79,9 +86,10 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById(operand1).textContent = operand1;
-    document.getElementById(operand2).textContent = operand2;
-    document.getElementById("operator").textContent = "+";
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
@@ -92,4 +100,3 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
 
 }
-
